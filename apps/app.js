a5982d86eae4ebb83217dc6899a5d410865399dd
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-07-01 12:25:01 -0600

    orginzed code into a MVC style

--- apps/app.js
+++ apps/app.js
@@ -1,44 +1,60 @@
 $(document).ready(function() {
-	$(function(){
-		$('form').submit(function() {
-			event.preventDefault();
-			var searchTerm = $('.search-term').val();
-			getRequest(searchTerm);
-		});
-	})
+	'use strict';
 
-	function getRequest(searchTerm) {
-		var params = {
-			part: 'snippet',
-			key: 'AIzaSyDHCMF4v4Nf78cmYalbUCd46r_4HFCdMHw',
-			q: searchTerm,
-			r: 'json'
-		};
-		url = 'https://www.googleapis.com/youtube/v3/search';
+	var controller = {
+		init: function() {
+			view.init();
+		},
 
-		$.getJSON(url, params, function(data) {
-			console.log(data);
-			showResults(data.items);
-		});
+		searchResults: function(searchTerm) {
+			model.getRequest(searchTerm);
+		},
+
+		generateResults: function(results) {
+			view.showResults(results)
+		}
 	}
 
-	function showResults(results) {
-		var ul = '',
-			videoTitle = '',
-			videoId = '',
-			channelTitle = '',
-			channelId = '',
-			thumbnail = {};
+	var model = {
+		getRequest: function(searchTerm) {
+			var params = {
+				part: 'snippet',
+				key: 'AIzaSyDHCMF4v4Nf78cmYalbUCd46r_4HFCdMHw',
+				q: searchTerm,
+				r: 'json'
+			},
+				url = 'https://www.googleapis.com/youtube/v3/search';
 
-		$.each(results, function(index, value) {
-			ul += '<li><div class="video-result"><div class="thumbnail"><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '"><img src=' + value.snippet.thumbnails.medium.url + ' alt=' + value.snippet.title + '></a></div><div class="videoInfo"><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '">' + value.snippet.title + '</a><a href="https://www.youtube.com/channel/' + value.snippet.channelId +'">' + value.snippet.channelTitle + '</a><p>' + value.snippet.description + '</p></div></div></li>';
-	videoTitle += '<p>' + value.Title + '</p>';
-			console.log(value.Title);
-		});
-
-		$('ul').empty().append(ul);
+			$.getJSON(url, params, function(results) {
+				console.log(results);
+				controller.generateResults(results.items);
+			});
+		}
 	}
-	$('ul').on('click', 'img', function() {
+	
+	var view = {
+		init: function() {
+			$('form').submit(function() {
+				event.preventDefault();
+				var searchTerm = $('.search-term').val();
+				controller.searchResults(searchTerm);
+			});
+		},
 
-	});
+		showResults: function(results) {
+			var ul = '';
+
+			$.each(results, function(index, value) {
+				ul += '<li><div class="video-result"><div class="thumbnail"><a href="https://www.youtube.com/watch?v=' +
+					value.id.videoId + '"><img src=' + value.snippet.thumbnails.medium.url + ' alt=' + value.snippet.title +
+					'></a></div><div class="videoInfo"><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '">' +
+					value.snippet.title + '</a><a href="https://www.youtube.com/channel/' + value.snippet.channelId +'">' +
+					value.snippet.channelTitle + '</a><p>' + value.snippet.description + '</p></div></div></li>';
+			});
+
+			$('ul').empty().append(ul);
+		}
+	};
+
+	controller.init();
 });
